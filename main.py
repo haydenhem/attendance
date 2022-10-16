6e77ec4c416cf06e7ec2201226f99f46e2b6e863
hayden <myemail@gmail.com>
2022-10-16 19:47:35 -0400

    made a check for if im at school or nah

--- main.py
+++ main.py
@@ -4,27 +4,36 @@ import base64 # For when i create class codes (REALLY UN-NEEDED DID IT FOR THE L
 
 classname = ""
 # This just makes sure its defined before I set it.
-
+directory = ""
+def does_drive_exist(letter):
+    import win32file
+    return (win32file.GetLogicalDrives() >> (ord(letter.upper()) - 65) & 1) != 0
+if does_drive_exist("H"):
+    directory = "H://Classes/"
+    print("Drive H: exists")
+else:
+    directory = "/Classes/"
+    print("Drive H: does not exist")
 def setup(): # This is the class creator / setup
     classname = input("What is the name of the class you want to set up? ")
-    os.mkdir("H://Classes/" + classname) # Makes the folder for the class | If you see H:// its cause of school computers
+    os.mkdir(directory + classname) # Makes the folder for the class | If you see H:// its cause of school computers
 
     studentammount = int(input("How many students? ")) # Asks this so I dont have to always ask if you are done after every name
     studentammountpt2 = 0 # Used for while loop. Essentailly just takes the number of stundents mentioned and if it isnt that it adds one to this number.
     students = [] # Creates student list (Soon to be set.)
-    f = open("H://Classes/" + classname + "/people.txt", "w") # Creates the file for the list of students, and opens it to be written.
+    f = open(directory + classname + "/people.txt", "w") # Creates the file for the list of students, and opens it to be written.
     while studentammount >= studentammountpt2:
         if studentammountpt2 == studentammount:
             studentammountpt2 += 1
             print("You are done filling out student names. The names you have saved are: ") # Double check u didnt typo on someones name
             print(students) # Prints the list of students.
-            pog = input("Do you want to restart? (Y/N) ") 
+            pog = input("Do you want to restart? (Y/N) ")
             if any(pog.lower() == f for f in ["yes", 'y', '1', 'ye']):
-                print("Restarting...") 
+                print("Restarting...")
                 studentammountpt2 = 0 # Clears the amount of ppl gone through
                 students.clear() # Clears the list. and than sends you back to the begining.
             if any(pog.lower() == l for l in ['no', 'n', '0']):
-                print("Saving to file...") 
+                print("Saving to file...")
                 f.write(','.join(students)) # Makes it easier to read later.
                 f.close() # Closes access to the people.txt file
                 yasgn = input("Do you want to copy the class code? (Y/N) ")
@@ -38,14 +47,14 @@ def setup(): # This is the class creator / setup
 
 
 def classcodes(classname):  # creates the class code and returns with the b64 string
-    f = open("H://Classes/" + classname + "/people.txt", "r")
+    f = open(directory + classname + "/people.txt", "r")
     deez = classname + "," + f.read()
     f.close()
     osad = base64.b64encode(deez.encode("ASCII"))
     return osad.decode("ASCII")
 
 
-def readfromandsep(textfile, seperator): # reads from a file than seperates it by the seperator (BY ME)
+def readfromandsep(textfile, seperator): # reads from a file than separates it by the seperator (BY ME)
     f = open(textfile, "r")
     return f.read().split(seperator)
 
@@ -54,8 +63,8 @@ def seperatetext(text, seperator): # Same as last but without the opening of fil
     return text.split(seperator)
 
 
-def attendance(): 
-    pog = readfromandsep("H://Classes/" + classname + "/people.txt", ",") # Takes the file and seperates the "NAME,NAME,NAME" format.
+def attendance():
+    pog = readfromandsep(directory + classname + "/people.txt", ",") # Takes the file and seperates the "NAME,NAME,NAME" format.
     # students = len(pog) # reads number of items
     attendance = [] # creates the list for the "NAME is here, NAME isnt here" format
     for i in range(len(pog)): # for each item in the list it asks if they are here
@@ -64,30 +73,30 @@ def attendance():
             attendance.append(" " + pog[i] + " is here")
         if any(yes.lower() == l for l in ['no', 'n', '0']):
             attendance.append(" " + pog[i] + " isnt here")
-    r = open("H://Classes/" + classname + "/attendance" + str(date.today()) + ".txt", "w") # Creates file and writes too it
+    r = open(directory + classname + "/attendance" + str(date.today()) + ".txt", "w") # Creates file and writes too it
     r.write("Date: " + str(date.today()) + "\n") # Adds the date for easy reading
     r.write(','.join(attendance)) # Writes the list of the "NAME is here, NAME isnt here" format
     r.close() # closes so it doesnt mem leak
-    print("Saved attendance for the day to Classes/" + classname + "/attendance" + str(date.today()) + ".txt") # Just tells you a quick link to your files
+    print("Saved attendance for the day to " + directory + classname + "/attendance" + str(date.today()) + ".txt") # Just tells you a quick link to your files
     exit(1) # program is done, nothing more after this point
 
 
-if os.path.exists("H://Classes") == False: # If my classes file doesnt exist than make it
-    os.mkdir("H://Classes")
+if os.path.exists(directory) == False: # If my classes file doesnt exist than make it
+    os.mkdir(directory)
 
 
 gaming = input("Would you like to input a classcode? (Y/N) ")
 if any(gaming.lower() == f for f in ["yes", 'y', '1', 'ye']):
     troll = input("Please paste it now: ") # Class codes are formated like: "CLASSNAME,NAME,NAME,NAME" than encrypted for looks, legit would work without but it looks cooler this way
     pogaming = seperatetext(base64.b64decode(troll.encode("ASCII")).decode("ASCII"), ",") # This took me way too long for 1 line, Essentially when you need to encode or decode a b64 string in python you have to encode it to bytes than decode it than conver those bytes to a string.
-    if not pogaming[0] in os.listdir("H://Classes"):
-        os.mkdir("H://Classes/" + pogaming[0]) # Make the class file if it doesnt exist, they just allows you to overwrite classes
-    f = open("H://Classes/" + pogaming[0] + "/people.txt", "w")
+    if not pogaming[0] in os.listdir(directory):
+        os.mkdir(directory + pogaming[0]) # Make the class file if it doesnt exist, they just allows you to overwrite classes
+    f = open(directory + pogaming[0] + "/people.txt", "w")
     f.write(','.join(pogaming[:0] + pogaming[0 + 1:])) # Adds every item in the list except the first item (Removes the classname from the students list)
     f.close()
     print("Students: "+','.join(pogaming[:0] + pogaming[0 + 1:]))
-    
-dir = os.listdir("H://Classes") # had to move this because after inputing a classcode it still thought the directory was empty
+
+dir = os.listdir(directory) # had to move this because after inputing a classcode it still thought the directory was empty
 if len(dir) == 0: # Checks if the directory for classes is empty if it is it sends you to setup a class
     # print("Empty directory")
     setup()
@@ -102,7 +111,7 @@ if len(dir) >= 1: # If your directory has 1 or more items than ask if you want t
                 blicky = input("Do you want to copy the class code? (Y/N) ")
                 if any(blicky.lower() == f for f in ["yes", 'y', '1', 'ye']):
                     print("Your Class code is: " + classcodes(classname))
-                attendance() # sends you to the attendance def 
+                attendance() # sends you to the attendance def
     if any(pog.lower() == f for f in ['no', 'n', '0']):
         setup()
 
